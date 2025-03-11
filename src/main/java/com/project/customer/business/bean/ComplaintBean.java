8a8b4b3ed38ed88c4d4846239bb046629429c4d0
anotherfaizan <mdfaizankhan2002@gmail.com>
2025-03-11 13:31:30 +0530

    Adds Validation Constraints

--- src/main/java/com/project/customer/business/bean/ComplaintBean.java
+++ src/main/java/com/project/customer/business/bean/ComplaintBean.java
@@ -1,18 +1,29 @@
 package com.project.customer.business.bean;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Date;
+import jakarta.validation.constraints.NotNull;
 
 public class ComplaintBean {
 
     private Integer complaintId;
 
+    @NotBlank(message = "Please Enter Customer Name.")
     private String customerName;
     private String description;
 
+    @NotNull(message = "Please Enter Date of Incidence.")
+    @Past(message = "Date of Incidence must be past date.")
+    @DateTimeFormat(pattern = "dd-MMM-yyyy")
     private Date dateOfIncidence;
 
+    @NotNull(message = "Please mention total amount of loss you have suffered through this incident.")
     private Double amount;
 
+    @NotNull(message = "Please Enter Customer Type.")
     private Integer complaintTypeId;
 
     public Integer getComplaintId() {
